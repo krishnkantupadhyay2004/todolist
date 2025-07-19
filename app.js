c72a8678695adc45387381c3505bc8595ebb91ac
krishnkantupadhyay2004 <krishnkant.sjp2004@gmail.com>
2025-07-19 20:06:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,18 +12,34 @@ let i = document.querySelector(".addbtn");
 
 
 
-i.addEventListener("click",function(){
+i.addEventListener("click",function(event){
     let inputtext = send.value;
     if(inputtext != ""){
       let p = document.createElement("p");
        p.innerText = inputtext;
        let li = document.createElement("li");
        li.append(p);
+       let delbtn = document.createElement("button");
+       delbtn.innerText = "delete";
+       delbtn.classList.add("delbtn");
+       li.append(delbtn);
+      
        taskadd.append(li);
        send.value="";
-       console.log(inputtext);
+      
        send.style.display="none";
        btn.style.display = "none";
        addtask.style.display="block";
 }
 })
+taskadd.addEventListener("click",function(event){
+   
+   if(event.target.nodeName == "BUTTON"){
+    let listitem = event.target.parentElement;
+    listitem.remove();
+     console.log(event.target.parentElement);
+   } else{
+    console.log("press on delete");
+   }
+})
+
